a7a3d063b016196c6a4da15a855d9e57513b1a45
Negati-ve <codezed@icloud.com>
2021-06-08 19:51:30 +0530

    fix asin mismatch

--- amazonFunctions.js
+++ amazonFunctions.js
@@ -1,101 +1,134 @@
-var log = require('loglevel');
-log.setLevel('debug')
+var log = require("loglevel");
+log.setLevel("debug");
 findPinnedAOD = async (amazonPage) => {
-    try {
-        let name;
-        let price;
-        // amazonPage.waitFor(1000)
-        const noBuyBox = await amazonPage.$$('[class="a-offscreen"]');
-        // console.log(noBuyBox, noBuyBox != null, noBuyBox.length == 0)
-        if (noBuyBox != null && noBuyBox.length == 0) {
-            console.log("no buybox", noBuyBox)
-            return { name: 'not avaiable', price: 'not available' };
-        }
-        await amazonPage.waitForXPath('//*[@id="aod-pinned-offer-additional-content"]');
-        const pinnedAOD = await amazonPage.$$('[id="aod-pinned-offer"]');
-        for (let AOD of pinnedAOD) {
-            price = await AOD.$eval('.a-offscreen', el => el.innerText);
-            name = await AOD.$eval('#aod-offer-soldBy > div > div >  div:nth-child(2) >a', el => el.innerText);
-        }
-        // await amazonPage.waitFor(1000)
-        return { name, price }
-    } catch (e) {
-        console.log(e)
+  try {
+    let name;
+    let price;
+    // amazonPage.waitFor(1000)
+    const noBuyBox = await amazonPage.$$('[class="a-offscreen"]');
+    // console.log(noBuyBox, noBuyBox != null, noBuyBox.length == 0)
+    if (noBuyBox != null && noBuyBox.length == 0) {
+      console.log("no buybox", noBuyBox);
+      return { name: "not avaiable", price: "not available" };
     }
-}
+    await amazonPage.waitForXPath(
+      '//*[@id="aod-pinned-offer-additional-content"]'
+    );
+    const pinnedAOD = await amazonPage.$$('[id="aod-pinned-offer"]');
+    for (let AOD of pinnedAOD) {
+      price = await AOD.$eval(".a-offscreen", (el) => el.innerText);
+      name = await AOD.$eval(
+        "#aod-offer-soldBy > div > div >  div:nth-child(2) >a",
+        (el) => el.innerText
+      );
+    }
+    // await amazonPage.waitFor(1000)
+    return { name, price };
+  } catch (e) {
+    console.log(e);
+  }
+};
 findPinnedAODDirect = async (amazonPage) => {
-    try {
-        let name;
-        let price;
-        // amazonPage.waitFor(1000)
-        const noBuyBox = await amazonPage.$$('[class="a-offscreen"]');
-        if (noBuyBox != null && noBuyBox.length != 0) {
-            console.log("no buybox", noBuyBox)
-            return { name: 'NA', price: 'NA' };
-        }
-        await amazonPage.waitForXPath('//*[@id="aod-pinned-offer-additional-content"]');
-        const pinnedAOD = await amazonPage.$$('[id="aod-pinned-offer"]');
-        for (let AOD of pinnedAOD) {
-            price = await AOD.$eval('.a-offscreen', el => el.innerText);
-            name = await AOD.$eval('#aod-offer-soldBy > div > div >  div:nth-child(2) >a', el => el.innerText);
-        }
-        // await amazonPage.waitFor(1000)
-        return { name, price }
-    } catch (e) {
-        console.log(e)
+  try {
+    let name;
+    let price;
+    // amazonPage.waitFor(1000)
+    const noBuyBox = await amazonPage.$$('[class="a-offscreen"]');
+    if (noBuyBox != null && noBuyBox.length != 0) {
+      console.log("no buybox", noBuyBox);
+      return { name: "NA", price: "NA" };
     }
-}
+    await amazonPage.waitForXPath(
+      '//*[@id="aod-pinned-offer-additional-content"]'
+    );
+    const pinnedAOD = await amazonPage.$$('[id="aod-pinned-offer"]');
+    for (let AOD of pinnedAOD) {
+      price = await AOD.$eval(".a-offscreen", (el) => el.innerText);
+      name = await AOD.$eval(
+        "#aod-offer-soldBy > div > div >  div:nth-child(2) >a",
+        (el) => el.innerText
+      );
+    }
+    // await amazonPage.waitFor(1000)
+    return { name, price };
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 findAODOfferList = async (amazonPage) => {
-    try {
-        let offers = []
-        await amazonPage.waitForXPath('//*[@id="aod-container"]');
-        const noOffers = await amazonPage.$$('[class="aod-no-offer-normal-font"]');
+  try {
+    let offers = [];
+    await amazonPage.waitForXPath('//*[@id="aod-container"]');
+    const noOffers = await amazonPage.$$('[class="aod-no-offer-normal-font"]');
 
-        if (noOffers != null && noOffers.length != 0) {
-            console.log("no offers", noOffers)
-            offers.push({ name: "NA", price: "NA" })
-            return offers;
-        }
-        const pinnedAOD = await amazonPage.$$('[id="aod-offer"]');
-        for (let AOD of pinnedAOD) {
-            let price;
-            let name;
-            price = await AOD.$eval('.a-offscreen', el => el.innerText);
-            name = await AOD.$eval('#aod-offer-soldBy > div > div >  div:nth-child(2) >a', el => el.innerText);
-            offers.push({ name, price })
-        }
-        return offers
-    } catch (e) {
-        console.log(e)
+    if (noOffers != null && noOffers.length != 0) {
+      console.log("no offers", noOffers);
+      offers.push({ name: "NA", price: "NA" });
+      return offers;
     }
-}
+    const pinnedAOD = await amazonPage.$$('[id="aod-offer"]');
+    for (let AOD of pinnedAOD) {
+      let price;
+      let name;
+      price = await AOD.$eval(".a-offscreen", (el) => el.innerText);
+      name = await AOD.$eval(
+        "#aod-offer-soldBy > div > div >  div:nth-child(2) >a",
+        (el) => el.innerText
+      );
+      offers.push({ name, price });
+    }
+    return offers;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 findAODOfferListDirect = async (amazonPage) => {
-    try {
-        let offers = []
-        await amazonPage.waitForXPath('//*[@id="aod-container"]');
-        const noOffers = await amazonPage.$$('[class="aod-no-offer-normal-font"]');
-        if (noOffers != null && noOffers.length == 0) {
-            console.log("no offers", noOffers)
-            return offers;
-        }
-
-        const pinnedAOD = await amazonPage.$$('[id="aod-offer"]');
-        for (let AOD of pinnedAOD) {
-            let price;
-            let name;
-            price = await AOD.$eval('.a-offscreen', el => el.innerText);
-            name = await AOD.$eval('#aod-offer-soldBy > div > div >  div:nth-child(2) >a', el => el.innerText);
-            offers.push({ name, price })
-        }
-        return offers
-    } catch (e) {
-        console.log(e)
+  try {
+    let offers = [];
+    await amazonPage.waitForXPath('//*[@id="aod-container"]');
+    const noOffers = await amazonPage.$$('[class="aod-no-offer-normal-font"]');
+    if (noOffers != null && noOffers.length == 0) {
+      console.log("no offers", noOffers);
+      return offers;
     }
-}
+
+    const pinnedAOD = await amazonPage.$$('[id="aod-offer"]');
+    for (let AOD of pinnedAOD) {
+      let price;
+      let name;
+      price = await AOD.$eval(".a-offscreen", (el) => el.innerText);
+      name = await AOD.$eval(
+        "#aod-offer-soldBy > div > div >  div:nth-child(2) >a",
+        (el) => el.innerText
+      );
+      offers.push({ name, price });
+    }
+    return offers;
+  } catch (e) {
+    console.log(e);
+  }
+};
+checkIfAsinMatch = async (amazonPage, asin) => {
+  try {
+    const hiddenAsin = await amazonPage.evaluate(() => {
+      const input = document.querySelector("#ddmSelectAsin");
+      return input.value;
+    });
+    console.log(hiddenAsin);
+    if (hiddenAsin == asin) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
 module.exports = {
-    findPinnedAOD,
-    findAODOfferList,
-    findAODOfferListDirect
-}
+  findPinnedAOD,
+  findAODOfferList,
+  findAODOfferListDirect,
+  checkIfAsinMatch,
+};
--- index.js
+++ index.js
@@ -1,146 +1,143 @@
-const amazonFunctions = require('./amazonFunctions.js');
-const ObjectsToCsv = require('objects-to-csv')
-const puppeteer = require('puppeteer');
+const amazonFunctions = require("./amazonFunctions.js");
+const ObjectsToCsv = require("objects-to-csv");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
 var bluebird = require("bluebird");
-// var Xvfb = require('xvfb');
-var log = require('loglevel');
-log.setLevel('info');
+var log = require("loglevel");
+log.setLevel("info");
 var count = 0;
-// return 1;
 var skip = 0;
 let newCSV = [];
 
 (async () => {
+  const csvInputFilePath = "./input.csv";
+  const csvToJson = require("csvtojson");
 
-    const csvInputFilePath = './input.csv'
-    const csvToJson = require('csvtojson')
+  const csvData = await csvToJson().fromFile(csvInputFilePath);
+  let ASINS = csvData.map((val) => val.ASIN);
 
-    const csvData = await csvToJson().fromFile(csvInputFilePath);
-    let ASINS = csvData.map(val => val.ASIN)
+  const get = async (ASIN, currentCount) => {
+    try {
+      let start = new Date();
 
-    const get = async (ASIN, currentCount) => {
-        try {
-            let start = new Date()
+      let browser = await puppeteer.launch({
+        headless: false,
+        ignoreHTTPSErrors: true,
+        userDataDir: "./data",
+        args: [
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          "--disable-infobars",
+          "--window-position=0,0",
+          "--ignore-certifcate-errors",
+          "--ignore-certifcate-errors-spki-list",
+          '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
+        ],
+      });
 
-            let browser = await puppeteer.launch({
-                headless: true, args: [
-                    '--no-sandbox',
-                    '--headless',
-                    '--disable-gpu',
-                    '--window-size=1920x1080'
-                ]
-            });
+      let productUrl = `https://www.amazon.in/dp/${ASIN}/#aod`;
+      let url = `https://www.amazon.in/gp/aod/ajax/ref=dp_aod_NEW_mbc?asin=${ASIN}`;
+      let lowestPrice = 99999999999;
+      let newCSVRow = {
+        ASIN: "",
+        url: "",
+        buy_box_seller_name: "",
+        buy_box_seller_price: "",
+        lowest_price_seller_name: "",
+        lowest_price_seller_price: "",
+        seller_2_name: "",
+        seller_2_price: "",
+        seller_3_name: "",
+        seller_3_price: "",
+        seller_4_name: "",
+        seller_4_price: "",
+        seller_5_name: "",
+        seller_5_price: "",
+        seller_6_name: "",
+        seller_6_price: "",
+      };
+      let seller_count = 0;
+      const page = await browser.newPage();
 
-            // const page = await browser.newPage();
-            // await page.setViewport({
-            //     width: 1200,
-            //     height: 800
-            // });
+      await page.setViewport({
+        width: 1080,
+        height: 1080,
+      });
+      // await page.setRequestInterception(true);
 
+      // page.on("request", (req) => {
+      //   if (
+      //     req.resourceType() == "stylesheet" ||
+      //     req.resourceType() == "font" ||
+      //     req.resourceType() == "image"
+      //   ) {
+      //     req.abort();
+      //   } else {
+      //     req.continue();
+      //   }
+      // });
 
-            let productUrl = `https://www.amazon.in/dp/${ASIN}/#aod`
-            let url = `https://www.amazon.in/gp/aod/ajax/ref=dp_aod_NEW_mbc?asin=${ASIN}&m=&pinnedofferhash=&qid=&smid=&sourcecustomerorglistid=&sourcecustomerorglistitemid=&sr=&pc=dp`
-            let lowestPrice = 99999999999;
-            let newCSVRow = {
-                ASIN: '',
-                url: '',
-                buy_box_seller_name: '',
-                buy_box_seller_price: '',
-                lowest_price_seller_name: '',
-                lowest_price_seller_price: '',
-                seller_2_name: '',
-                seller_2_price: '',
-                seller_3_name: '',
-                seller_3_price: '',
-                seller_4_name: '',
-                seller_4_price: '',
-                seller_5_name: '',
-                seller_5_price: '',
-                seller_6_name: '',
-                seller_6_price: '',
-            }
-            let seller_count = 0;
-            const page = await browser.newPage();
-            await page.setViewport({
-                width: 1080,
-                height: 1080
-            });
-            await page.setRequestInterception(true);
+      let x = await page.goto(url);
 
-            page.on('request', (req) => {
-                if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
-                    req.abort();
-                }
-                else {
-                    req.continue();
-                }
-            });
+      let asinMatch = await amazonFunctions.checkIfAsinMatch(page, ASIN);
+      if (!asinMatch) {
+        console.log(`ASIM MISMATCH for ${ASIN}`);
+        return 0;
+      }
+      let buybox = await amazonFunctions.findPinnedAOD(page);
+      let offers = await amazonFunctions.findAODOfferList(page);
 
-            // console.log()
-            // console.log(count, 'EXTRACTING ', url)
-            let x = await page.goto(url)
-            // viewProfileButtonSelector = '#olp_feature_div > div:nth-child(4) > span:nth-child(1) > a:nth-child(1) > span:nth-child(1)'
-            // await page.waitForSelector(viewProfileButtonSelector)
-            // let button = await page.$(viewProfileButtonSelector)
-            // await button.click()
-            // console.log(x)
-            let buybox = await amazonFunctions.findPinnedAOD(page)
-            let offers = await amazonFunctions.findAODOfferList(page)
-            // console.log(buybox)
-            newCSVRow.ASIN = ASIN
-            newCSVRow.url = productUrl
+      newCSVRow.ASIN = ASIN;
+      newCSVRow.url = productUrl;
 
-            newCSVRow.buy_box_seller_name = buybox.name
-            newCSVRow.buy_box_seller_price = buybox.price
+      newCSVRow.buy_box_seller_name = buybox.name;
+      newCSVRow.buy_box_seller_price = buybox.price;
 
-            if (lowestPrice > parseInt(buybox.price.replace(/[^\d.-]/g, ''))) {
-                newCSVRow[`lowest_price_seller_name`] = buybox.name
-                newCSVRow[`lowest_price_seller_price`] = parseInt(buybox.price.replace(/[^\d.-]/g, ''))
-                lowestPrice = parseInt(buybox.price.replace(/[^\d.-]/g, ''))
-            }
-            offers.forEach((offer, index) => {
-                newCSVRow[`seller_${index + 2}_name`] = offer.name
-                newCSVRow[`seller_${index + 2}_price`] = offer.price
-                if (lowestPrice > parseInt(offer.price.replace(/[^\d.-]/g, ''))) {
-                    newCSVRow[`lowest_price_seller_name`] = offer.name
-                    newCSVRow[`lowest_price_seller_price`] = parseInt(offer.price.replace(/[^\d.-]/g, ''))
-                    lowestPrice = parseInt(buybox.price.replace(/[^\d.-]/g, ''))
-                }
-            })
-            await page.waitFor(1000)
-            newCSV.push(newCSVRow)
-            let csv = new ObjectsToCsv([newCSVRow])
-            await csv.toDisk('./output.csv', { append: true })
-            let end = new Date() - start
-            console.log(currentCount, 'EXTRACTED ', ASIN)
-            console.info('Execution time: %dms', end)
-            await page.close()
-            await browser.close();
-            return 1
-        } catch (e) {
-            // await page.close()
-            // await browser.close();
-            console.log('FAILED', currentCount, ASIN)
-            console.log(e)
+      if (lowestPrice > parseInt(buybox.price.replace(/[^\d.-]/g, ""))) {
+        newCSVRow[`lowest_price_seller_name`] = buybox.name;
+        newCSVRow[`lowest_price_seller_price`] = parseInt(
+          buybox.price.replace(/[^\d.-]/g, "")
+        );
+        lowestPrice = parseInt(buybox.price.replace(/[^\d.-]/g, ""));
+      }
+      offers.forEach((offer, index) => {
+        newCSVRow[`seller_${index + 2}_name`] = offer.name;
+        newCSVRow[`seller_${index + 2}_price`] = offer.price;
+        if (lowestPrice > parseInt(offer.price.replace(/[^\d.-]/g, ""))) {
+          newCSVRow[`lowest_price_seller_name`] = offer.name;
+          newCSVRow[`lowest_price_seller_price`] = parseInt(
+            offer.price.replace(/[^\d.-]/g, "")
+          );
+          lowestPrice = parseInt(buybox.price.replace(/[^\d.-]/g, ""));
         }
+      });
+      await page.waitFor(1000);
+      newCSV.push(newCSVRow);
+      let csv = new ObjectsToCsv([newCSVRow]);
+      await csv.toDisk("./output.csv", { append: true });
+      let end = new Date() - start;
+      console.log(currentCount, "EXTRACTED ", ASIN);
+      console.info("Execution time: %dms", end);
+      await page.close();
+      await browser.close();
+      return 1;
+    } catch (e) {
+      console.log("FAILED", currentCount, ASIN);
+      console.log(e);
     }
-    await bluebird.map(ASINS, async (asin) => {
-        count = count + 1;
-        if (count < skip) {
-            return;
-        }
-        await get(asin, count)
-    }, { concurrency: 50 });
-    // process.on('SIGINT', async () => {
-    //     console.log("Caught interrupt signal");
-    //     const csv = new ObjectsToCsv(newCSV)
-    //     await csv.toDisk('./output.csv')
-    //     console.log("Finish")
-    //     if (i_should_exit)
-    //         process.exit();
-    // });
+  };
+  await bluebird.map(
+    ASINS,
+    async (asin) => {
+      count = count + 1;
+      if (count < skip) {
+        return;
+      }
+      await get(asin, count);
+    },
+    { concurrency: 50 }
+  );
 
-    console.log("Finish")
-
-})()
-
+  console.log("Finish");
+})();
